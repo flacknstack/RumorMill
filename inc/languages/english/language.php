27d01dfaa539ac1d49658109d119a939424541c9
Stack <gambill.staci@gmail.com>
2026-03-08 19:10:34 -0400

    Create language.php

---
+++ inc/languages/english/language.php
@@ -0,0 +1,51 @@
+<?php
+$l['gossip_main'] = "Rumor mill";
+$l['gossip_main_welcome'] = "Welcome to the rumor mill";
+
+/*
+Language variable for the form
+*/
+
+$l['gossip_text'] = "The rumor";
+$l['gossip_group'] = "Affected group";
+$l['gossip_victim'] = "Affected character(s)";
+$l['gossip_date'] = "Date of the rumor";
+$l['gossip_go'] = "To spread a rumor";
+$l['gossip_open'] = "Be patient, the rumor mill is checking your new rumor for accuracy.";
+
+/*
+ * Optionen
+ */
+
+$l['gossip_delete'] = "Delete";
+$l['gossip_edit'] = "edit";
+
+/*
+ * Index
+ */
+
+$l['gossip_index'] = "latest rumors";
+$l['gossip_link'] = "<a href='misc.php?action=gossip'>View all rumors</a>";
+$l['gossip_nogossip'] = "No rumors have been circulating recently.";
+$l['gossip_gossipabout'] = "A rumor about ";
+/*
+ * Sprachvariabel modcp
+ */
+$l['gossip_modcp_nav'] = "Manage rumor mill";
+$l['gossip_modcp_newalert'] = "New rumors have been circulating. Click here to check if you can send them out into the world..";
+$l['gossip_modcp'] = "Management system rumor mill";
+$l['gossip_modcp_new'] = "New rumors";
+$l['gossip_modcp_gossipabout'] = "A rumor about";
+$l['gossip_modcp_accept'] = "Accept rumor";
+$l['gossip_modcp_refuse'] = "Reject the rumor";
+$l['gossip_modcp_nogossip'] = "Currently no rumors have been rejected.";
+$l['gossip_modcp_all'] = "All the rumors";
+/*
+ * Alerts
+ */
+$l['gossip_alert'] = "A new rumor about you has been spread!";
+$l['gossip_ok'] = "{1} has accepted your rumor.";
+$l['gossip_refuse'] = "{1} Your rumor has been rejected. If you have any questions, please contact the team.";
+$l['myalerts_setting_gossip_alert'] = "Would you like to be notified when a rumor about you is spread?";
+$l['myalerts_setting_gossip_ok'] = "Would you like to be notified when your rumor has been accepted?";
+$l['myalerts_setting_gossip_refused'] = "Would you like to be notified if your rumor was rejected?";
